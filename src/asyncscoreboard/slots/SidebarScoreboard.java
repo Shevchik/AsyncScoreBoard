0c079ae35b69460388d1dae8f08bce468668aa2f
Shevchik <Shev4ik.den@gmail.com>
2016-10-28 21:37:57 +0300

    Actually format sidebar title

--- src/asyncscoreboard/slots/SidebarScoreboard.java
+++ src/asyncscoreboard/slots/SidebarScoreboard.java
@@ -15,7 +15,7 @@ public class SidebarScoreboard {
 	private static final char[] colorCodes = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 	public static void updateScoreBoard(Player player) {
-		Scoreboards.getInstance().updateSlot(player, DisplaySlot.SIDEBAR, Main.cfgSBSidebarTitle, slot -> {
+		Scoreboards.getInstance().updateSlot(player, DisplaySlot.SIDEBAR, Scoreboards.getSidebarTitle(player), slot -> {
 			String[] newRows = Scoreboards.getSidebarLines(player);
 			Set<String> oldRows = slot.getEntries();
 			if (oldRows.isEmpty()) {
